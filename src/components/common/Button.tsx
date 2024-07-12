8ce36bb46a17384bbaa22555f9e70078527abd84
assasinooz77 <assasinooz77@gmail.com>
2024-07-12 21:13:10 +0800

    feat: add landing faq

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,6 +3,7 @@ import {
   Button as MtButton,
   ButtonProps,
   PolymorphicComponentProps,
+  Text,
 } from "@mantine/core";
 
 type ButtonType = PolymorphicComponentProps<"button", ButtonProps>;
@@ -10,14 +11,16 @@ type ButtonType = PolymorphicComponentProps<"button", ButtonProps>;
 export const Button = ({ className, children, ...props }: ButtonType) => {
   return (
     <MtButton
-      className={`!text-black !px-6 !py-2 !nunito-semibold ${className}`}
+      className={`${className}`}
       variant="primary"
       tt="uppercase"
-      size="sm"
+      size="md"
       radius={5}
       {...props}
     >
-      {children}
+      <Text size="sm" fw={600} className="!nunito-semibold" c="black">
+        {children}
+      </Text>
     </MtButton>
   );
 };
--- src/components/ui/Landing/index.tsx
+++ src/components/ui/Landing/index.tsx
@@ -1,9 +1,11 @@
+import { LandingFaq } from "./LandingFaq";
 import { LandingMain } from "./LandingMain";
 
 export const Landing = () => {
   return (
     <>
       <LandingMain />
+      <LandingFaq />
     </>
   );
 };
